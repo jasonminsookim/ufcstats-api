43394160c088e11489e49026322188a33d324047
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2022-06-15 00:25:36 +0000

    Add event location to models

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ class Event(Base):
     __tablename__ = "events"
     event_name = Column(String, index=True)
     event_date = Column(DateTime, index=True)
+    event_location = Column(String, index=True)
     event_url = Column(String, index=True, primary_key=True)
     datetime_scraped = Column(DateTime, index=True)
 
